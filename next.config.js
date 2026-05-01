7ba8b7defb4410f61d7cacb1c2d96aca4135095a
Talnet Thrive Solutions <talnetthrivesolutions@gmail.com>
2026-05-01 22:21:52 +0530

    chore: finalized azure deployment settings and rotated secrets

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   // Silences the next-pwa webpack-config conflict in Next.js 16
   turbopack: {},
